9e4f66d8ea5c3aa54c81b8fe5075252f1b6fa228
artoria95 <apbarua95@gmail.com>
2024-05-14 19:31:29 -0300

    generating components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GeneratorComponent } from "./generator/generator.component";
+import { ToolbarComponent } from "./toolbar/toolbar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, GeneratorComponent, ToolbarComponent]
 })
 export class AppComponent {
   title = 'angular-generators-generator';
