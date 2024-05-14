9cbd8734cd520cdc0b724b679c501f59c3d002ac
artoria95 <apbarua95@gmail.com>
2024-05-14 20:47:46 -0300

    page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { GeneratorComponent } from './generator/generator.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {path: 'crisis-list', component: GeneratorComponent},
     {path: 'heroes-list', component: ToolbarComponent},
     {path: '', redirectTo: '/heroes-list', pathMatch: 'full'},
+    {path: '**', component: PageNotFoundComponent}
 ];
